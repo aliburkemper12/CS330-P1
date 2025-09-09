0f054d3eaaea33f2b8df82f4bfc7e9020fe5269e
aliburkemper12 <aliburkemper12@gmail.com>
2025-09-09 09:55:55 -0500

    initial commit

---
+++ fall25-project1-aeburkemper/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace fall25_project1_aeburkemper.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
